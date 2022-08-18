86e1c1919d1202f7c626b37db6a1e27445ea269c
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2022-08-18 18:25:45 +0530

    Refactor and Extract method for year only divisible by 4 only

--- src/main/java/com/incubyte/LeapYearService.java
+++ src/main/java/com/incubyte/LeapYearService.java
@@ -2,9 +2,13 @@ package com.incubyte;
 
 public class LeapYearService {
     public boolean isLeapYear(int year) {
-        if (year == 2008 || year == 2012 || year == 2016 || year % 400 == 0) {
+        if (isDivisibleBy4Only(year) || year % 400 == 0) {
             return true;
         }
         return false;
+    }
+
+    private boolean isDivisibleBy4Only(int year) {
+        return year == 2008 || year == 2012 || year == 2016;
     }
 }
