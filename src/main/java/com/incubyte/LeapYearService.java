e258d114e75836f55cc7e254f7e31ec41f256a64
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2022-08-18 18:44:22 +0530

    Refactor

--- src/main/java/com/incubyte/LeapYearService.java
+++ src/main/java/com/incubyte/LeapYearService.java
@@ -2,9 +2,11 @@ package com.incubyte;
 
 public class LeapYearService {
 
+    private static final String NEGATIVE_YEAR_NOT_ALLOWED_PATTERN =  "A year cannot be negative: %s. Did you mean %s?";
+
     public boolean isLeapYear(int year) {
         if (year < 0) {
-            throw new IllegalArgumentException("A year cannot be negative: " + year + ". Did you mean " + Math.abs(year) + "?");
+            throw new IllegalArgumentException(String.format(NEGATIVE_YEAR_NOT_ALLOWED_PATTERN, year, Math.abs(year)));
         }
         if (isDivisibleBy100AndNotBy400(year)) {
             return false;
