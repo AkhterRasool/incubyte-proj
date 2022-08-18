43ca21d74d3a7601450c758a821a4b5a61329a03
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2022-08-18 18:29:06 +0530

    Make existing tests pass after refactor

--- src/main/java/com/incubyte/LeapYearService.java
+++ src/main/java/com/incubyte/LeapYearService.java
@@ -2,6 +2,9 @@ package com.incubyte;
 
 public class LeapYearService {
     public boolean isLeapYear(int year) {
+        if (isDivisibleBy100AndNotBy400(year)) {
+            return false;
+        }
         if (isDivisibleBy4Only(year) || year % 400 == 0) {
             return true;
         }
@@ -9,6 +12,10 @@ public class LeapYearService {
     }
 
     private boolean isDivisibleBy4Only(int year) {
-        return year == 2008 || year == 2012 || year == 2016;
+        return year % 4 == 0;
+    }
+
+    private boolean isDivisibleBy100AndNotBy400(int year) {
+        return year % 100 == 0 && year % 400 != 0;
     }
 }
