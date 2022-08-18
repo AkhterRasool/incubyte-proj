107cf29e367cc6fe533b235eacbaf11fd6d5b0c5
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2022-08-18 18:05:42 +0530

    Create test for checking if leap year is divisible by 400

--- src/test/java/com/incubyte/LeapYearServiceTest.java
+++ src/test/java/com/incubyte/LeapYearServiceTest.java
@@ -1,4 +1,15 @@
 package com.incubyte;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.AssertTrue.assertTrue;
+
 public class LeapYearServiceTest {
+
+    private LeapYearService subject = new LeapYearService();
+
+    @Test
+    void yearShouldBeLeapYearWhenDivisibleBy400() {
+        assertTrue(subject.isLeapYear(400));
+    }
 }
