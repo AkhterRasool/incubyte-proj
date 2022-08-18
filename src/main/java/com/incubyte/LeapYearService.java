c5dc5096e2f87aaad08efa4cebf1fb8cd2cf5669
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2022-08-18 18:35:46 +0530

    Refactor to make it readable

--- src/main/java/com/incubyte/LeapYearService.java
+++ src/main/java/com/incubyte/LeapYearService.java
@@ -5,14 +5,22 @@ public class LeapYearService {
         if (isDivisibleBy100AndNotBy400(year)) {
             return false;
         }
-        return isDivisibleBy4Only(year) || year % 400 == 0;
+        return isDivisibleBy4Only(year) || isDivisible(year, 400);
     }
 
     private boolean isDivisibleBy4Only(int year) {
-        return year % 4 == 0;
+        return isDivisible(year, 4);
     }
 
     private boolean isDivisibleBy100AndNotBy400(int year) {
-        return year % 100 == 0 && year % 400 != 0;
+        return isDivisible(year, 100) && isNotDivisible(year, 400);
+    }
+
+    private boolean isDivisible(int dividend, int divisor) {
+        return dividend % divisor == 0;
+    }
+
+    private boolean isNotDivisible(int dividend, int divisor) {
+        return !isDivisible(dividend, divisor);
     }
 }
